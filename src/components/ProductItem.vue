78daa612063f74b6cdaea1cc730910d178407c3b
AlexQuidditch <alexquidditch@gmail.com>
2019-01-29 17:51:18 +0300

    - компонент продукта - список продуктов

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <p>{{ product.name }}</p>
+    <a :href="product.link" target="_blank" rel="noopener">{{ product.name }}</a>
+    <p>{{ product.price }} руб.</p>
+    <button @click="addToCart" type="button">В корзину</button>
+  </li>
+</template>
+
+<script>
+
+export default {
+  name: 'Product-Item',
+  props: {
+    'product': {
+      type: Object,
+      required: true,
+    }
+  },
+
+  methods: {
+    addToCart () {
+      const { product } = this;
+      this.$bus.emit('add-product' , product);
+    }
+  },
+};
+
+</script>
+
+<style lang="scss"></style>

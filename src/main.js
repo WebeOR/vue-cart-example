e5499eaf99103b19a3de2fb93ce19393b98a5b47
AlexQuidditch <alexquidditch@gmail.com>
2019-01-29 17:49:30 +0300

    - зависимости

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSlideUpDown from 'vue-slide-up-down';
+// https://github.com/yangmingshan/vue-bus
+import VueBus from 'vue-bus';
 
+Vue.component('v-slide', VueSlideUpDown);
+Vue.use(VueBus);
 Vue.config.productionTip = false
 
 new Vue({
